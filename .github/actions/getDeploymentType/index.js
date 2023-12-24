fb5c556cdbe33659b3088daecb1c67f9eedb9c49
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-12-24 13:45:03 -0300

    a

---
+++ .github/actions/getDeploymentType/index.js
@@ -0,0 +1,22 @@
+const version = require('../package.json').version
+const core = require('@actions/core')
+
+try {
+  if (typeof version !== 'string') throw new Error('Invalid version')
+
+  /**
+   * @param {string} version
+   */
+  const getIsPatch = version => {
+    const parts = version.split('.')
+    return (
+      parts.length === 3 && parts.every(p => p.match(/\d+/)) && parts[2] !== '0'
+    )
+  }
+
+  const isPatch = getIsPatch(version)
+  console.log(isPatch)
+  core.setOutput('isUpdate', isPatch)
+} catch (error) {
+  core.setFailed(error.message)
+}
