348fec6392590260cfdeabc4d2b91120757ba4c5
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-12-24 11:30:39 -0300

    a

--- .deploy/action.ts
+++ .deploy/action.ts
@@ -1,6 +1,8 @@
 const version = require('../package.json').version
 
-const getIsPatch = version => {
+if (typeof version !== 'string') throw new Error('Invalid version')
+
+const getIsPatch = (version: string) => {
   const parts = version.split('.')
   return (
     parts.length === 3 && parts.every(p => p.match(/\d+/)) && parts[2] !== '0'
