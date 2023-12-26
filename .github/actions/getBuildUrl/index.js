dd447071729382cb7404475d4487e0b11d0e17a3
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-12-26 17:53:55 -0300

    a

--- .github/actions/getBuildUrl/index.js
+++ .github/actions/getBuildUrl/index.js
@@ -1,22 +1,27 @@
 const version = require('../../../package.json').version
 const core = require('@actions/core')
+const { execSync } = require('child_process')
 
 try {
   if (typeof version !== 'string') throw new Error('Version should be a string')
 
-  const buildListJson = core.getInput('buildListJson')
-  const buildList = JSON.stringify(buildListJson)
+  const platform = core.getInput('platform')
+  const profile = core.getInput('profile')
 
   console.log('Version:', version)
 
   /**
-   * @param {unknown[]} buildList
+   * @param {string} platform
+   * @param {profile} profile
    */
-  const getBuildUrl = buildList => {
+  const getBuildUrl = (platform, profile) => {
+    const command = `eas build:list --non-interactive --json --buildProfile ${profile} --platform ${platform}`
+    const buildListJson = execSync(command).toString()
+    const buildList = JSON.parse(buildListJson)
     return buildList.find(b => b.status === 'FINISHED')?.artifacts?.buildUrl
   }
 
-  const buildUrl = getBuildUrl(buildList)
+  const buildUrl = getBuildUrl(platform, profile)
   if (buildUrl === undefined) throw new Error('Build not found')
   console.log('Build URL:', buildUrl)
   core.setOutput('buildUrl', buildUrl)
