c3e52a9d1a052897df37754050db478df1ae18c3
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-12-26 22:32:00 -0300

    a

---
+++ .github/actions/getLastChangelog/index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+const fs = require('fs')
+
+try {
+  const getLastChangelog = () => {
+    const entire = fs.readFileSync('../../../CHANGELOG.md').toString()
+    const rows = entire.split('\n')
+    const tail = rows.slice(6, rows.length)
+    const endIndex = tail.findIndex(r => r.startsWith('## '))
+    return tail.slice(0, endIndex === -1 ? tail.length : endIndex).join('\n')
+  }
+
+  const lastChangelog = getLastChangelog()
+  if (lastChangelog === undefined) throw new Error('Build not found')
+  console.log('Last changelog:')
+  console.log(lastChangelog)
+  core.setOutput('lastChangelog', lastChangelog)
+} catch (error) {
+  core.setFailed(error.message)
+}
